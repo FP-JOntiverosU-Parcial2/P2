53a007b6f193d9749ab836ce08aef5ea9e25ae5f
jaime <jaime@vaio>
2016-10-13 11:16:19 -0500

    Fp-Menu version 2

---
+++ src/problema/pkg2/Problema2.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problema.pkg2;
+import java.util.*;
+/**
+ *
+ * @author jaime
+ */
+public class Problema2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int m, R, milla, yarda, pie;
+        m=solicitarMetros();
+        R=mostrarResultados(m);
+        
+        
+        
+        
+                
+    }
+    public static int solicitarMetros(){
+        int m;
+        System.out.println("Introduce los metros para convertirlos en pies, millas y yardas:");
+        Scanner teclado= new Scanner(System.in);
+        m=teclado.nextInt();
+        return m;
+   }
+    
+    
+
+    
+    public static int mostrarResultados(int m){
+        double pie = 3.28084;
+        double milla = 0.000621371;
+        double yarda = 1.09361;
+        float r1 = (float) (m*milla);
+        float r2 = (float) (m*yarda);
+        float r3 = (float) (m*pie);
+        System.out.println(m + " es igual a: " + r1);
+        System.out.println(m + " es igual a: " + r2);
+        System.out.println(m + " es igual a: " + r3);
+        return m;
+    }
+            
+}
