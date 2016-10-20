0f481bcfc01e5d6acabd5579b512fa8813bf03cf
jaime <jaime@vaio>
2016-10-20 14:54:15 -0500

    problema 2

--- src/problema/pkg2/Problema2.java
+++ src/problema/pkg2/Problema2.java
@@ -16,9 +16,9 @@ public class Problema2 {
      */
     public static void main(String[] args) {
        int m, R, milla, yarda, pie;
-        m=solicitarMetros();
-        R=mostrarResultados(m);
-        
+        m=solicitarMetros();    //se solicitan los metros
+        R=mostrarResultados(m); //se muestran la operacion y el resultado
+        mensajeSalida(); //se agradece por usar el programa y sale del programa
         
         
         
@@ -36,16 +36,19 @@ public class Problema2 {
 
     
     public static int mostrarResultados(int m){
-        double pie = 3.28084;
+        double pie = 3.28084;    //Se introduce el valor de las millas, pulgadas y yardas
         double milla = 0.000621371;
         double yarda = 1.09361;
-        float r1 = (float) (m*milla);
-        float r2 = (float) (m*yarda);
-        float r3 = (float) (m*pie);
+        double r1 =  m*milla;   //Se hacen las operaciones correspondientes
+        double r2 = m*yarda;
+        double r3 = m*pie;
         System.out.println(m + " metros" + " es igual a: " + r1 + " milla(s)");
-        System.out.println(m + " metros" + " es igual a: " + r2 + " yarda(s");
+        System.out.println(m + " metros" + " es igual a: " + r2 + " yarda(s)");
         System.out.println(m + " metros" + " es igual a: " + r3 + " pie(s)");
         return m;
     }
-            
+    public static void mensajeSalida(){
+        System.out.println("Gracias por usar el programa"); //se agradece
+        System.exit(0);//Sale del programa
+    }       
 }
